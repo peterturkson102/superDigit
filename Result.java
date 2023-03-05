66f0a7bba8b3cc518ec5e6869e835d7b91fa4d67
Peter Kofi Turkson Asamoah <43896389+peterturkson102@users.noreply.github.com>
2023-03-05 02:31:13 +0000

    Create Result.java

---
+++ Result.java
@@ -0,0 +1,31 @@
+class Result {
+
+    /*
+     * Complete the 'superDigit' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters: 
+     *  1. STRING n
+     *  2. INTEGER k
+     */
+
+    public static int superDigit(String n, int k) {
+    // Write your code here
+         String p =n;
+         
+         while(p.length()*k>1){
+             int sum = 0;
+             for(int i = 0;i<p.length();i++){
+                 sum+= Character.getNumericValue(p.charAt(i)) ;
+             }
+             sum = sum*k;
+             p= String.valueOf(sum);
+             k=1;
+         }
+         int superD = Integer.valueOf(p);
+         
+         
+    return superD;
+    }
+
+}
